7123ff7c1eb59d53d4375b54917ee24bc11f2625
GFernande7 <gikon145@yandex.ru>
2024-04-30 14:40:38 +0300

    Setting up the admin panel for bugs and features

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+def create_status_action(status, description):
+    @admin.action(description=description)
+    def status_action(modeladmin, request, queryset):
+        queryset.update(status=status)
+    status_action.__name__ = f'set_status_{status}'    
+    return status_action
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('title', 'project', 'task')}),
+        ('Bug Details', {'fields': ('status', 'priority', 'description')}),
+        ('Dates', {'fields': ('created_at', 'updated_at'), 'classes': ('collapse',)}),
+    )
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+    list_editable = ('status', 'priority')
+    readonly_fields = ('created_at', 'updated_at')
+    actions = [
+        create_status_action('New', 'Переместить в статус "Новая"'),
+        create_status_action('In_progress', 'Переместить в статус "В работе"'),
+        create_status_action('Completed', 'Переместить в статус "Завершена"'),
+    ]
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('title', 'project', 'task')}),
+        ('Request Details', {'fields': ('status', 'priority', 'description')}),
+        ('Dates', {'fields': ('created_at', 'updated_at'), 'classes': ('collapse',)})
+    )
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+    list_editable = ('status', 'priority')
+    readonly_fields = ('created_at', 'updated_at')
