c41db33a42cbfa53a94b0c416078b3c40dfbdbd8
GFernande7 <gikon145@yandex.ru>
2024-04-30 21:26:37 +0300

    Created templates for bugs and features

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -5,16 +5,16 @@ app_name = 'quality_control'
 
 urlpatterns = [
     #-------------------------------Function-Based Views------------------------------------------
-    # path('', views.index, name='index'),
-    # path('bugs/', views.bug_list, name='bug_list'),
-    # path('features/', views.feature_list, name='feature_list'),
-    # path('bugs/<int:bug_id>/', views.bug_detail, name='bug_id'),
-    # path('features/<int:feature_id>/', views.feature_detail, name='feature_id'),
+    path('', views.index, name='index'),
+    path('bugs/', views.bug_list, name='bug_list'),
+    path('features/', views.feature_list, name='feature_list'),
+    path('bugs/<int:bug_id>/', views.bug_detail, name='bug_id'),
+    path('features/<int:feature_id>/', views.feature_detail, name='feature_id'),
 
     #---------------------------------Class-Based Views-------------------------------------------
-    path('', views.IndexView.as_view(), name='index'),
-    path('bugs/', views.BugsListView.as_view(), name='bug_list'),
-    path('features/', views.FeaturesListView.as_view(), name='feature_list'),
-    path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_id'),
-    path('features/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_id'),
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('bugs/', views.BugsListView.as_view(), name='bug_list'),
+    # path('features/', views.FeaturesListView.as_view(), name='feature_list'),
+    # path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_id'),
+    # path('features/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_id'),
 ]
