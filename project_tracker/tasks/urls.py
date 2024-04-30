afe1f10585d3991155c30d1a15f8b58336ca01ee
GFernande7 <gikon145@yandex.ru>
2024-04-30 20:35:13 +0300

    Created templates for menu and tasks

--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -5,13 +5,13 @@ app_name = 'tasks'
 
 urlpatterns = [
 
-    # path('', views.index),
-    # path('projects/', views.projects_list, name='projects_list'),
-    # path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
-    # path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('', views.index, name='index'),
+    path('projects/', views.projects_list, name='projects_list'),
+    path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
+    path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
     
-    path('', views.IndexView.as_view(), name='index'),
-    path('projects/', views.ProjectsListView.as_view(), name='projects_list'),
-    path('projects/<int:project_id>/', views.ProjectDetailView.as_view(), name='project_detail'),
-    path('projects/<int:project_id>/tasks/<int:task_id>/', views.TaskDetailView.as_view(), name='task_detail'),
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('projects/', views.ProjectsListView.as_view(), name='projects_list'),
+    # path('projects/<int:project_id>/', views.ProjectDetailView.as_view(), name='project_detail'),
+    # path('projects/<int:project_id>/tasks/<int:task_id>/', views.TaskDetailView.as_view(), name='task_detail'),
 ]
